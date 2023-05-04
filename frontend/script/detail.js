959ba5d72fdd968b246fb34233dfcc08a841f671
PhilippeMath49 <philippe.mathieu@ynov.com>
2023-05-04 10:06:38 +0200

    avancement detail

--- frontend/script/detail.js
+++ frontend/script/detail.js
@@ -6,6 +6,9 @@ const url = "http://" + ip_add + ":" + port;
 let itemDetails;
 let item;
 
+const itemImages= document.querySelector(".item-images");
+const itemText= document.querySelector("items-detail"); 
+
 StartingFunc();
 
 function StartingFunc() {
@@ -36,3 +39,45 @@ function GetItemDetailsFromAPI() {
 function DisplayItemDetails() {
     document.querySelector(".title").innerHTML = item.name;
 }
+
+function DisplayImages(){
+    itemImages.innerHTML="";
+    let imageList = document.createElement("form")
+    let selectedValue= document.querySelector(".image-list input:checked").value;
+    imageList.classList.add("image-list")
+
+    let HtmlList;
+    for (let i = 0; i < item.images[color].length; i++){
+        if (i===0){
+            HtmlList+= `<input type="radio" value="${item.images[color][i]}" name="image"  checked>
+                        <label for="image"><img src="${item.images[color][i]}" alt="item image ${i}"></label>`
+        }else{
+            HtmlList+= `<input type="radio" value="${item.images[color][i]}" name="image">
+                        <label for="image"><img src="${item.images[color][i]}" alt="item image ${i}"></label>`
+        }
+    }
+    imageList.innerHTML+=HtmlList;
+
+    let mainImage = document.createElement("div");
+    mainImage.classList.add("main-image");
+    let HtmlMainImage= `<img src="${selectedValue}" alt="item image">`
+    mainImage.innerHTML+=HtmlMainImage;
+    
+
+    itemImages.appendChild(imageList);
+    itemImages.appendChild(mainImage);
+}
+
+function DisplayText(){
+    //itemText.innerHTML="";
+    let itemDescription = document.createElement("p");
+    itemDescription.classList.add("item-description");
+
+    let HtmlText = `${item.description}`
+    itemDescription.innerHTML+=HtmlText;
+    itemText.appendChild(itemDescription)
+}
+
+DisplayText();
+
+DisplayImages();
